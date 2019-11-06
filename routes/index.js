9cfe5418d6e2ccc117be82b2b1ece21dba216e01
Daniel Nieto <danielnieto89@gmail.com>
2019-11-06 16:27:50 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var offset = 200;
+var images = generateArrayOfImages(500)
+var methods = ['none', 'native', 'lozad', 'lazyload', 'lazysizes']
+
+function generateArrayOfImages(count) {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    result.push(`https://picsum.photos/id/${i + offset}/1000/1000`);
+  }
+
+  return result;
+}
+
+router.get('/', function (req, res, next) {
+  res.render('index', {methods});
+});
+
+router.get('/:method', function (req, res, next) {
+  const method = req.params.method;
+
+  if (!methods.includes(method)) {
+    res.send(404);
+  }
+
+  res.render(method, { showInfo: true, images, info: method});
+});
+
+module.exports = router;
